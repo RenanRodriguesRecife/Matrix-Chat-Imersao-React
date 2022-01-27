62c1d73b6162c6254fd41c232be3ab352aa28d23
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-27 01:36:23 -0300

    global style fit height

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,24 @@ function GlobalStyle(){
                 margin: 0;
                 padding: 0;
                 box-sizing: border-box;
-               
+                list-style:none;
             }
             body{
-                font-family: sans-serif;
+                font-family: 'Open Sans', sans-serif;
             }
+            /* App fit height */
+            html, body, #__next{
+                min-height: 100vh;
+                display:flex;
+                flex:1;
+            }
+            #__next{
+                flex: 1;
+            }
+            #__next>*{
+                flex:1;
+            }
+            /* ./App fit height */
         `}</style>
     )
 }
