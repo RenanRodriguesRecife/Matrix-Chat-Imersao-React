89e91b4c5db3818a5956fbc5797b28cec2a06698
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-27 00:34:07 -0300

    style jsx

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 function HomePage(){
-    return <div>Welcome to Next.js!</div>
+
+    return (
+    <div>
+        <h1>Boas vinda de volta!</h1>
+        <h2>Discord - Alura Matrix</h2>
+        
+        <style jsx>{`
+            h1{
+                color: red;
+            }
+        `}</style>
+        </div>
+    )
 }
 
 export default HomePage
