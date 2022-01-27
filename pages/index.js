13fa89bbe61a009f289bd7366ec82bab4c2e22e9
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-27 01:01:46 -0300

    props tag parse and css dinamic change

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,28 @@
 function Title(props){
     console.log(props.children);
+    const Tag = props.tag;
     return(
+    <>
         <h1>{props.children}</h1>
+        <style jsx>{`
+        ${Tag}{
+            color: red;
+            font-size:24px;
+            font-weight:600;
+        }
+    `}</style>
+    </>
     );
 }
 
 function HomePage(){
 
     return (
-    <div>
-        <Title>Boas vinda de volta!</Title>
+    <>
+        <Title tag="h1">Boas vinda de volta!</Title>
         <h2>Discord - Alura Matrix</h2>
         
-        <style jsx>{`
-            h1{
-                color: red;
-            }
-        `}</style>
-        </div>
+    </>
     )
 }
 
